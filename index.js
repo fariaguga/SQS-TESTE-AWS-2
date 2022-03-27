6231aa306b796095e7359d9edb526eb4b30c3e68
Faria <fariaguga@hotmail.com>
2022-03-27 19:09:23 -0300

    fix 5

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const { User } = require('./models')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(bodyParser.json(), res.header("Acess-Control-Allow-Origin", "*"));
 app.use(bodyParser.json(), function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
