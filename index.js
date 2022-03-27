2b56ab6bfa8b83c74d0cccb16d494248d9b33a00
Faria <fariaguga@hotmail.com>
2022-03-27 18:06:27 -0300

    fix: cors route origin acess

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 // app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${port} </h1>`));
 
-app.post('/user', cors({ origin: "https://arabela-backend.herokuapp.com/"}), async (req, res) => {
+app.post('/user', cors({ origin: "https://arabela-banda.herokuapp.com/"}), async (req, res) => {
 
   const { name, email, wpp, music } = req.body;
 
